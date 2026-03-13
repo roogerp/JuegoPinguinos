ccdb3f6737e6e0c50c801dde40c888d5c431a38c
roger <maitebst@alumnes.ilerna.com>
2026-03-13 19:19:41 +0100

    controlador
    controlador gestor BBDD

---
+++ JuegoPinguinos/src/CONTROLADOR/GestorBBDD.java
@@ -0,0 +1,43 @@
+package CONTROLADOR;
+
+import MODELO.*;
+
+public class GestorBBDD {
+
+    private String urlBBDD;
+    private String username;
+    private String password;
+
+    public String getUrlBBDD() {
+        return urlBBDD;
+    }
+
+    public void setUrlBBDD(String urlBBDD) {
+        this.urlBBDD = urlBBDD;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void guardarBBDD(Partida p) {
+        // TODO: implementar guardado en base de datos
+    }
+
+    public Partida cargarBBDD(int id) {
+        // TODO: implementar carga desde base de datos
+        return null;
+    }
+}
